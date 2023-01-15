bb4ff0a426647d99f0ebd3446681e5e59bb72116
nishan2003481 <67799738+nishan2003481@users.noreply.github.com>
2023-01-16 04:03:31 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -7,7 +7,7 @@ from config import ADMIN
 async def start_cmd(bot, msg):
     txt="This is personal use bot 🙏. Do you want your own bot? 👇 Click the source code to deploy"
     btn = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/MrMKN/Simple-Rename-Bot")
+        InlineKeyboardButton("🤖 SOURCE CODE", url="https://github.com/nishan2003481/Simple-Rename-Bot101")
         ],[
         InlineKeyboardButton("🖥️ How To Deploy", url="https://youtu.be/oc847WvOUaI")
     ]])
